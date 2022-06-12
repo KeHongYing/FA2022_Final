3e6dfb3cb2f4fb29f4face204d19b4197a0394a8
laxingyang <seediqbale880212@gmail.com>
2022-06-12 14:08:01 +0800

    add backend

---
+++ back-end/CalcResourceFactory.cpp
@@ -0,0 +1,85 @@
+#include "CalcResourceFactory.h"
+
+#include <sstream>
+#include <iomanip>
+#include "json.hpp"
+
+using namespace nlohmann;
+
+CalcResourceFactory::CalcResourceFactory()
+{
+    _resource = make_shared<Resource>();
+    _resource->set_path(
+        "/{optionType: put|call}"
+        "/{optionStyle: american|europe|bermuda}"
+        "/{spotPrice: [-+]?[0-9]*\\.?[0-9]*}"
+        "/{strikePrice: [-+]?[0-9]*\\.?[0-9]*}"
+        "/{interestRate: [-+]?[0-9]*}"
+        "/{volatility: [-+]?[0-9]*}"
+        "/{matureDate: [-+]?[0-9]*}"
+        "/{periods: [-+]?[0-9]*}");
+    _resource->set_method_handler("GET",
+                                  [&](const auto session)
+                                  {
+                                      get_handler(session);
+                                  });
+}
+
+shared_ptr<Resource> CalcResourceFactory::get_resource() const
+{
+    return _resource;
+}
+
+float CalcResourceFactory::calculate(float num1, float num2, string operation)
+{
+    return 123;
+    if (operation == "add")
+    {
+        return num1 + num2;
+    }
+    else if (operation == "subtract")
+    {
+        return num1 - num2;
+    }
+    else if (operation == "multiply")
+    {
+        return num1 * num2;
+    }
+    else if (operation == "divide")
+    {
+        return num1 / num2;
+    }
+}
+
+tuple<string, string, double, double, double, double, int, int> CalcResourceFactory::get_path_parameters(
+    const shared_ptr<Session> session) const
+{
+    const auto &request = session->get_request();
+    const auto optionType = request->get_path_parameter("optionType");
+    const auto optionStyle = request->get_path_parameter("optionStyle");
+    auto spotPrice = atof(request->get_path_parameter("spotPrice").c_str());
+    auto strikePrice = atof(request->get_path_parameter("strikePrice").c_str());
+    auto interestRate = atof(request->get_path_parameter("interestRate").c_str()) / 100.0;
+    auto volatility = atof(request->get_path_parameter("volatility").c_str()) / 100.0;
+    auto matureDate = atof(request->get_path_parameter("matureDate").c_str());
+    auto periods = atof(request->get_path_parameter("periods").c_str());
+    return make_tuple(optionType, optionStyle, spotPrice, strikePrice, interestRate, volatility, matureDate, periods);
+}
+
+string CalcResourceFactory::to_json(float result)
+{
+    ostringstream str_stream;
+    str_stream << result;
+    json jsonResult = {
+        {"result", str_stream.str()}};
+    return jsonResult.dump();
+}
+
+void CalcResourceFactory::get_handler(const shared_ptr<Session> session)
+{
+    const auto [optionType, optionStyle, spotPrice, strikePrice, interestRate, volatility, matureDate, periods] = get_path_parameters(session);
+    auto result = calculate(optionType, optionStyle, spotPrice, strikePrice, interestRate, volatility, matureDate, periods);
+    auto content = to_json(result);
+    session->close(OK, content,
+                   {{"Content-Length", to_string(content.size())}});
+}
